b9cb68a64e637e0c32bdb0650052f8f6e3b43af1
fancccc <51230867+fancccc@users.noreply.github.com>
2019-11-19 11:00:29 +0800

    Create wechat组合版.py

---
+++ wechat组合版.py
@@ -0,0 +1,97 @@
+# wechat-
+#最终版
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 16:48:10 2019
+
+@author: Mario
+"""
+import itchat
+from itchat.content import *
+import random,readjosn_myself
+
+data = readjosn_myself.readjosn_myself(r"e:/Python/work/BullshitGenerator-master/data.json")
+ana = data["famous"] # a 代表前面垫话，b代表后面垫话
+before_word = data["before"] # 在名人名言前面弄点废话
+after_word = data['after']  # 在名人名言后面弄点废话
+rubbish = data['bosh'] # 代表文章主要废话来源
+time0 = 2
+
+def traverse(lis):
+    global time0
+    pool = list(lis) * time0
+    while True:
+        random.shuffle(pool)
+        for i in pool:
+            yield i
+
+next_rub = traverse(rubbish)
+next_ana = traverse(ana)
+
+def get_ana():
+    global next_ana
+    xx = next(next_ana)
+    xx = xx.replace(  "a",random.choice(before_word) )
+    xx = xx.replace(  "b",random.choice(after_word) )
+    return xx
+
+def next_para():
+    xx = ". "
+    xx += "\r\n"
+    xx += "    "
+    return xx
+tmp = str()
+def send_msg(get_msg):
+    xx = str(get_msg)
+    for x in xx:
+        tmp = str()
+        while ( len(tmp) < 300 ) :
+            branch = random.randint(0,100)
+            if branch < 10:
+                tmp += next_para()
+            elif branch < 40 :
+                tmp += get_ana()
+            else:
+                tmp += next(next_rub)
+        tmp = tmp.replace("x",xx)
+        return tmp
+
+def mass_texting(word):
+    friends = itchat.get_friends(update=True)[0:]
+    for i in range(len(friends)):
+        to_id = friends[i]['UserName']
+        itchat.send_msg(word,toUserName=to_id)
+        
+sta1=''
+sta2=''
+
+@itchat.msg_register(TEXT)  #这里的TEXT表示如果有人发送文本消息，那么就会调用下面的方法
+def simple_reply(msg):
+  #这个是向发送者发送消息
+  #itchat.send_msg('已经收到了文本消息，消息内容为%s'%msg['Text'],toUserName=msg['FromUserName'])
+  friends = itchat.get_friends(update=True)[0:]
+  if msg['Content']=='群发1010110':
+      global sta1,sta2
+      sta1='群发'
+      return 'please input mass texting '
+  elif msg['Content']=='取消群发1010110':
+      sta1=''
+      return '已取消 '
+  elif sta1=='群发':
+      mass_texting(msg['Content'])
+      sta1=''
+      return '群发成功'
+  elif msg['Content']=='废话机器1010110':
+      sta2='启动'
+      return '启动成功'
+  elif msg['Content']=='关闭1010110':
+      sta2=''
+      return '关闭成功'
+  elif sta2=='启动':
+      get_msg = msg['Content']
+      tosend_word = send_msg(get_msg)
+      return tosend_word  #返回的给对方的消息，msg["Text"]表示消息的内容
+
+
+itchat.auto_login(hotReload=True)
+itchat.run()
